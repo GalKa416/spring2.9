e1b428b52ab31272f42abf1d83ca8598514d8b35
Галина <g416@mail.ru>
2023-07-30 11:46:44 +0500

    шаг1

---
+++ src/main/java/com/example/storewebadd/StoreWebAddApplication.java
@@ -0,0 +1,13 @@
+package com.example.storewebadd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreWebAddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreWebAddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/storewebadd/StoreWebAddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storewebadd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreWebAddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
